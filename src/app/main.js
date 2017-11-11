33d7d95101e4a225b44a95959723b8b69a7c5afe
davidan90 <david16an@gmail.com>
2017-11-11 13:56:23 +0100

    add store to reduxService

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 //Components
 import App from './app.component';
@@ -8,18 +7,8 @@ import App from './app.component';
 // Services
 import { reduxService } from './services/redux';
 
-const store = createStore(
-    reduxService.reducers.all,
-    (localStorage['redux-store']) ? JSON.parse(localStorage['redux-store']) : {},
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-store.subscribe( () => {
-    localStorage['redux-store'] = JSON.stringify(store.getState());
-});
-
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={reduxService.store}>
         <App />
     </Provider>, 
     document.getElementById('app-root')
--- src/app/services/redux/index.js
+++ src/app/services/redux/index.js
@@ -1,7 +1,30 @@
+import { createStore } from 'redux';
+
 import * as actions from './actions';
 import * as reducers from './reducers';
+
+const getStore = () => {
+    const store = createStore(
+        reducers.all,
+        (localStorage['redux-store']) ? JSON.parse(localStorage['redux-store']) : {},
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    
+    store.subscribe( () => {
+        localStorage['redux-store'] = JSON.stringify(store.getState());
+    });
+
+    return store;
+}
+
+const select= () => {
+    /* TODO get observable property */
+}
+
+const store = getStore();
 
 export const reduxService = {
     actions,
     reducers,
+    store,
 };
