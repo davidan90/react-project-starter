d2907da773d031ba37951d964667592af6b443b2
davidan90 <david16an@gmail.com>
2017-11-22 14:02:07 +0100

    update NavbarMovile

--- src/app/components/navbar/components/navbar-mobile.component.js
+++ src/app/components/navbar/components/navbar-mobile.component.js
@@ -1,5 +1,30 @@
-export const NavbarMobile = () => (
-    <div className="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 openAside">
-        <span>...</span>
-    </div>
-);
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+// Services
+import { AppActions } from '../../../services/redux/actions';
+
+let openAside = false;
+
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = (dispatch) => ({
+    clickAction: () => {
+        openAside = !openAside;
+        dispatch(AppActions.openAside(openAside));
+    },
+});
+
+@connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)
+export class NavbarMobile extends Component {
+    render() {
+        const {clickAction} = this.props;
+        return (
+            <div className="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 openAside">
+                <span onClick={clickAction}>...</span>
+            </div>
+        );
+    }
+}
