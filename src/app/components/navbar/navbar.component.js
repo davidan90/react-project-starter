9a8f38adbd82934192249f2fd1152f2de61eba49
davidan90 <david16an@gmail.com>
2017-11-28 14:01:16 +0100

    change navbar when resize

--- src/app/components/navbar/navbar.component.js
+++ src/app/components/navbar/navbar.component.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
 import { object } from 'prop-types';
 
-// Decorators
-import { ResponsiveComponent } from '../../decorators/responsive-component.decorator';
+// Services
+import { AppActions } from '../../services/redux/actions';
 
 // Components
 import { NavbarDesktop } from './components/navbar-desktop.component';
@@ -11,7 +12,20 @@ import { NavbarMobile } from './components/navbar-mobile.component';
 // Styles
 import './_navbar.component.scss';
 
-@ResponsiveComponent()
+const mapStateToProps = (state, props) => ({
+    isMobile: state.app.device.type === 'mobile',
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    closeAsideAction: () => {
+        dispatch(AppActions.openAside(false));
+    },
+});
+
+@connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)
 export class NavbarComponent extends Component {
     static propTypes = {
         links: object.isRequired,
@@ -21,8 +35,13 @@ export class NavbarComponent extends Component {
         links: {},
     }
 
-    _selectNavbar({ isDesktop, isTablet, isMobile, links }) {
-        return (isDesktop || isTablet) ? <NavbarDesktop links={links} /> : <NavbarMobile />;
+    _selectNavbar({ isMobile, links }) {
+        if (!isMobile) {
+            const { closeAsideAction } = this.props;
+            closeAsideAction();
+            return <NavbarDesktop links={links} />
+        }
+        return <NavbarMobile />
     }
 
     render() {
